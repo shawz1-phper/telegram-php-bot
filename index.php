1d4b1b317e67e094b8cb7ce562a68a8c1de6851a
shawz1-phper <ywaftkhr971@gmail.com>
2025-07-30 22:01:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $token = '8416879937:AAGaScKAEj5OIQxDy_Yfgi1TOngHpWVq678';
 $update = json_decode(file_get_contents('php://input'), true);
-$massage = $update['message']['text'] ?? '';
+$message = $update['message']['text'] ?? '';
 $chat_id = $update['message']['chat']['id'] ?? '';
-if ($massage == '/start') {
+if ($message == '/start') {
     sendMessage($chat_id, "أهلاً بك في بوت PHP على Railway!");
 }
 
-function sendMessage($chat_id, $massage) {
+function sendMessage($chat_id, $text) {
     $url = "https://api.telegram.org/bot$token/sendMessage";
     $data = ['chat_id' => $chat_id, 'text' => $text];
     file_get_contents($url . '?' . http_build_query($data));
